211f270ee6f9d94c03fbdd34f0944c43f24d0e72
Tom Shen <tom.shen@calix.com>
2025-03-10 10:41:16 +0800

    Allow download_files.sh use different DOWNLOAD_URL

--- download_files.sh
+++ download_files.sh
@@ -3,7 +3,7 @@
 
 CURL_UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
 
-DOWNLOAD_URL="https://immortalwrt.kyarucloud.moe"
+DOWNLOAD_URL=${DOWNLOAD_URL:-"https://immortalwrt.kyarucloud.moe"}
 if [ "$VERSION" == "snapshot" ]; then
 	DOWNLOAD_PATH="snapshots"
 else
