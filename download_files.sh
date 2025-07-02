165ed4a990f904f662e5bed8d0255d1d9b3f45b3
Tom Shen <tom.shen@calix.com>
2025-07-02 09:50:41 +0800

    Disable curl ua for download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Copyright (C) 2021 ImmortalWrt.org
 
-CURL_UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+# CURL_UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+CURL_UA=""
 
 DOWNLOAD_URL=${DOWNLOAD_URL:-"https://immortalwrt.kyarucloud.moe"}
 if [ "$VERSION" == "snapshot" ]; then
